26e30d6b7becd444be182e2d115541bb008c74d6
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-20 14:28:00 +0300

    Update CalculatingClassNUnitTests.cs

--- Day3Task1.NUnitTests/CalculatingClassNUnitTests.cs
+++ Day3Task1.NUnitTests/CalculatingClassNUnitTests.cs
@@ -1,28 +1,28 @@
-﻿using System;
+using System;
 using NUnit.Framework;
 
 namespace Day3Task1.NUnitTests
 {
     [TestFixture]
     public class CalculatingClassNUnitTests
-    {
-        [TestCase(1, 5, 0.0001, ExpectedResult = 1)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.001, 3, 0.0001, ExpectedResult = 0.1)]
-        [TestCase(0.04100625, 4, 0.0001, ExpectedResult = 0.45)]
-        [TestCase(8, 3, 0.0001, ExpectedResult = 2)]
-        [TestCase(0.0279936, 7, 0.0001, ExpectedResult = 0.6)]
-        [TestCase(0.0081, 4, 0.1, ExpectedResult = 0.3)]
-        [TestCase(-0.008, 3, 0.1, ExpectedResult = -0.2)]
-        [TestCase(0.004241979, 9, 0.00000001, ExpectedResult = 0.545)]
-        public double FindNthRootTest1(double number, double degree, double precision)
+    {        
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void FindNthRootTest1(double number, int degree, double precision, double result)
         {            
-            return CalculatingClass.FindNthRoot(number, degree, precision);
+            Assert.AreEqual(result, CalculatingClass.FindNthRoot(number, degree, precision), Math.Pow(10, 2 - precision.ToString().Length));
         }
 
-        [TestCase(8, 15, -7, -5)]
-        [TestCase(8, 15, -0.6, -0.1)]
-        public void FindNthRootTest_Number_Degree_Precision_ArgumentOutOfRangeException(double number, int degree, double precision, double expected)
+        [TestCase(8, 15, -7)]
+        [TestCase(8, 15, -0.6)]
+        public void FindNthRootTest_Number_Degree_Precision_ArgumentOutOfRangeException(double number, int degree, double precision)
         {
             Assert.Throws<ArgumentOutOfRangeException>(() => CalculatingClass.FindNthRoot(number, degree, precision));
         }
